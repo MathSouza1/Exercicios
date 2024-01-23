bde6c14917c358fe09ea176477d41651c98c41d9
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-01-23 19:43:54 -0300

    Adding polymorphism package

---
+++ src/oo/polimorfismo/Food.java
@@ -0,0 +1,19 @@
+package oo.polimorfismo;
+
+public abstract class Food {
+    private double weight;
+
+    public Food(double weight){
+        setWeight(weight);
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        if (weight >= 0){
+            this.weight = weight;
+        }
+    }
+}
